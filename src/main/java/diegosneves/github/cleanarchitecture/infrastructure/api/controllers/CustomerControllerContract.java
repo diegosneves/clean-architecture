037357f20d7b62957035c507187b7fdf9e47da68
Diego Neves <neves.diegoalex@outlook.com>
2024-04-18 21:13:28 -0300

    :building_construction: Adicione a interface CustomerControllerContract
    Esta atualização adiciona a interface CustomerControllerContract no pacote de controladores de API. Ela representa o contrato para o controlador de clientes, mas ainda precisa ser implementada.

---
+++ src/main/java/diegosneves/github/cleanarchitecture/infrastructure/api/controllers/CustomerControllerContract.java
@@ -0,0 +1,12 @@
+package diegosneves.github.cleanarchitecture.infrastructure.api.controllers;
+
+/**
+ * Esta interface representa o contrato para o controlador de clientes.
+ *
+ * @author diegoneves
+ */
+public interface CustomerControllerContract {
+
+    //TODO - Implementar...
+
+}
